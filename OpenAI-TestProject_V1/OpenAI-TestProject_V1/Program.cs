4fde290476fd680a283f24b38e7b732083918a4b
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2023-07-06 10:58:46 -0700

    Add files via upload
    Adding project to the repo

---
+++ OpenAI-TestProject_V1/OpenAI-TestProject_V1/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+
+// user asks a question
+var question = Console.ReadLine();
+
+// you're api key given to you
+var openAI = new OpenAIAPI("API Key Here");
+CompletionRequest completionRequest = new CompletionRequest();
+
+// calling on the completionrequest prompt here... the prompt is if your requesting a single prompt only
+completionRequest.Prompt = question;
+
+// getting the chat gpt - 3 model here
+completionRequest.Model = Model.DavinciText;
+
+// asking the API to complete the full request 
+var completions = openAI.Completions.CreateCompletionAsync(completionRequest);
+
+// printing out the anwser to the request
+foreach (var completion in completions.Result.Completions)
+{
+    Console.WriteLine(completion.Text);
+}
+
+Console.ReadLine();
